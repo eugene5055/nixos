5df1b6d77470a8f50e1b6cd1b5c3e66f81f6a280
eugene5055 <epeterside5055@gmail.com>
2026-01-12 21:12:29 -0500

    Update 2026 config: Fix pkgs-unstable and syntax errors

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   description = "GLF-OS ISO Configuration - Optimized 2026";
 
+
   inputs = {
-    glf-channels.url = "git+framagit.org";
+    glf-channels.url = "git+https://framagit.org/gaming-linux-fr/glf-os/channels-glfos/glf-os-channels.git?ref=main";
     nixpkgs.follows = "glf-channels/nixpkgs";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    glf.url = "git+framagit.org";
+    glf.url = "git+https://framagit.org/gaming-linux-fr/glf-os/glf-os.git?ref=main";
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote/master";
@@ -16,32 +17,25 @@
   outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, ... }:
     let
       system = "x86_64-linux";
+      # This block is MANDATORY for GLF-OS modules
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
-      # Use the 2026 official standard formatter
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
 
       nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
         inherit system;
-        # Passing 'inputs' is enough to access all dependencies in modules
-        specialArgs = { inherit inputs; };
-
+        # You MUST inherit pkgs-unstable here so modules can see it
+        specialArgs = { inherit inputs pkgs-unstable; };
         modules = [
           ./configuration.nix
           inputs.glf.nixosModules.default
           inputs.lanzaboote.nixosModules.lanzaboote
-
-          # Inline module for global settings and unstable overlay
           {
             nixpkgs.config.allowUnfree = true;
-            nixpkgs.overlays = [
-              (final: prev: {
-                unstable = import nixpkgs-unstable {
-                  inherit (final) system;
-                  config.allowUnfree = true;
-                };
-              })
-            ];
             nix.settings.experimental-features = [ "nix-command" "flakes" ];
           }
         ];
