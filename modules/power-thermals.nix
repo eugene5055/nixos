230787655a11c1b49586efe13218b1a46a0581be
eugene5055 <epeterside5055@gmail.com>
2026-01-19 10:20:43 -0500

    Resolve merge conflict in power-thermals.nix

--- modules/power-thermals.nix
+++ modules/power-thermals.nix
@@ -5,10 +5,15 @@
     cpuFreqGovernor = "performance";
   };
 
+<<<<<<< Updated upstream
   services.thermald.enable = false;
   services.irqbalance.enable = true;
   services.system76-scheduler.enable = true;
   services.lact.enable = false;
+=======
+  services.thermald.enable = true;
+  services.lact.enable = true;
+>>>>>>> Stashed changes
 
   zramSwap.enable = true;
 }
