5ebab75b1d761184193f754b0cf02a653287a6a5
eugene5055 <epeterside5055@gmail.com>
2026-01-19 10:36:09 -0500

    fixes

--- modules/power-thermals.nix
+++ modules/power-thermals.nix
@@ -5,22 +5,14 @@
     cpuFreqGovernor = lib.mkDefault "performance";
   };
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  services.thermald.enable = false;
-  services.irqbalance.enable = true;
-  services.system76-scheduler.enable = true;
-  services.lact.enable = false;
-=======
-  services.thermald.enable = true;
-  services.lact.enable = true;
->>>>>>> Stashed changes
-=======
+  # Thermal and IRQ Management
   services.thermald.enable = lib.mkDefault false;
   services.irqbalance.enable = lib.mkDefault true;
-  services.system76-scheduler.enable = lib.mkDefault true;
-  services.lact.enable = lib.mkDefault false;
->>>>>>> 60770a166adf54eac37c0f8e9dacd093c7dbf70e
 
+  # Scheduler and GPU Control (LACT)
+  services.system76-scheduler.enable = lib.mkDefault true;
+  services.lact.enable = lib.mkDefault true;
+
+  # Swap
   zramSwap.enable = lib.mkDefault true;
 }
