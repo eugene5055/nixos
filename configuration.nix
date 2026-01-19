7a2e09129246180a8d64e378a338fb02ebab2978
eugene5055 <epeterside5055@gmail.com>
2026-01-18 19:26:20 -0500

    update

--- configuration.nix
+++ configuration.nix
@@ -6,16 +6,11 @@
     ./customConfig
   ];
 
-  # --- Nix Package Manager Settings ---
   nix = {
     settings = {
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       experimental-features = [ "nix-command" "flakes" ];
-
-      # System features including x86_64-v3
-      system-features = [ "gccarch-x86-64-v3" "benchmark" "big-parallel" "kvm" "nixos-test" ];
-
       auto-optimise-store = true;
       download-buffer-size = 524288000;
     };
@@ -26,11 +21,9 @@
     };
   };
 
-  # --- Disable All Swap (File and Zram) ---
   swapDevices = [ ];
   zramSwap.enable = lib.mkForce false;
 
-  # --- Raptor Lake Hardware Support ---
   hardware.cpu.intel.updateMicrocode = true;
   hardware.graphics = {
     enable = true;
@@ -41,7 +34,7 @@
   glf.nvidia_config = {
     enable = true;
     laptop = false;
-    nvidiaBusId = "PCI:1:0:0"; # RTX 5090
+    nvidiaBusId = "PCI:1:0:0";
   };
 
   services.thermald.enable = true;
@@ -49,7 +42,6 @@
   services.flatpak.enable = true;
   powerManagement.cpuFreqGovernor = "performance";
 
-  # --- Kernel & Boot ---
   boot = {
     kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
 
@@ -82,16 +74,6 @@
     };
   };
 
-  # --- Platform Targeting x86_64-v3 ---
-  # This targets the v3 architecture level which Raptor Lake supports.
-  # Most Nix caches provide v3 binaries in 2026, so this won't force a full source rebuild.
-  nixpkgs.hostPlatform = lib.mkDefault {
-    system = "x86_64-linux";
-    gcc.arch = "x86-64-v3";
-    gcc.tune = "x86-64-v3";
-  };
-
-  # --- Overlays & Fixes ---
   nixpkgs.overlays = [
     (final: prev: {
       lha = prev.runCommand "lha-dummy" {} ''
@@ -103,7 +85,6 @@
     })
   ];
 
-  # --- System Environment & Gaming ---
   environment.systemPackages = with pkgs; [
     nvtopPackages.full
     btop
