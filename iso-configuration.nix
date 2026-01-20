8a0ae99e0a34c638b5c899ea063bc12387a089f1
eugene5055 <93231022+eugene5055@users.noreply.github.com>
2026-01-19 21:17:15 -0500

    Limit ISO filesystems to avoid zfs build

---
+++ iso-configuration.nix
@@ -0,0 +1,40 @@
+{ inputs, config, pkgs, lib, ... }: {
+  imports = [
+    ./modules/boot-kernel.nix
+    ./modules/nix-settings.nix
+    ./modules/nvidia.nix
+    ./modules/bluetooth-udev.nix
+    ./modules/power-thermals.nix
+    ./modules/systemd.nix
+    ./modules/desktop.nix
+    ./modules/audio.nix
+    ./modules/networking.nix
+    ./modules/users.nix
+    ./modules/packages.nix
+    ./modules/gaming.nix
+    ./modules/storage.nix
+    ./modules/state.nix
+  ];
+
+  boot = {
+    lanzaboote.enable = lib.mkForce false;
+    loader = {
+      systemd-boot.enable = lib.mkOverride 1000 true;
+      efi.canTouchEfiVariables = lib.mkForce false;
+    };
+    supportedFilesystems = lib.mkForce [
+      "btrfs"
+      "ext4"
+      "f2fs"
+      "vfat"
+      "xfs"
+    ];
+  };
+
+  isoImage = {
+    makeEfiBootable = true;
+    makeUsbBootable = true;
+  };
+
+  networking.hostName = lib.mkDefault "nixos-live";
+}
